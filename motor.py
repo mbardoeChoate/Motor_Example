5e1e4181e1885dbc273973d60dec8fbe3a1c8744
Matthew Bardoe <mbardoe@gmail.com>
2023-03-24 08:16:45 -0400

    Made a deadzone.

--- motor.py
+++ motor.py
@@ -3,6 +3,9 @@ class Motor:
         self.speed=0.5
 
     def set_speed(self, speed):
+        speed=max(-1, speed)
+        speed=min(1,speed)
+        speed=speed^3 # making a deadzone
         self.speed = speed
 
     def speed_up(self):
